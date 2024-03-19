cd079892b4d6ef0ecebed022f926735e59def4b5
Amir Ali Karimi <amr.karm_0121@yahoo.com>
2024-03-20 02:37:56 +0330

    Add predict sp500 without sentiment with RNN

---
+++ models/RNN.py
@@ -0,0 +1,88 @@
+from keras.layers import SimpleRNN, Dense
+from keras.models import Sequential
+from keras.losses import MeanSquaredError
+from keras.optimizers import Adam
+import matplotlib.pyplot as plt
+
+class RNN:
+	def __init__( self, neurons, batch_size, window_size, input_dim, stateful=True, unroll=True):
+		self.neurons = neurons
+		self.batch_size = batch_size
+		self.window_size = window_size
+		self.input_dim = input_dim
+		self.stateful = stateful
+		self.unroll = unroll
+
+
+		batch_input_shape = ( self.batch_size, self.window_size, self.input_dim)
+
+		model = Sequential( [
+			SimpleRNN(
+				neurons,
+				batch_input_shape=batch_input_shape,
+				stateful=stateful,
+				return_sequences=True,
+				unroll=unroll
+			),
+			SimpleRNN(
+				neurons,
+				return_sequences=False,
+				stateful=stateful,
+				unroll=unroll
+			),
+			Dense( 1)
+		])
+
+		model.compile(
+			loss=MeanSquaredError(),
+			optimizer=Adam()
+		)
+
+		self.model = model
+		self.history = None
+
+	def summary( self):
+		self.model.summary()
+
+	def fit( self, input, target, epochs, verbose=0):
+		self.history = self.model.fit(
+			input,
+			target,
+			epochs=epochs,
+			batch_size=self.batch_size,
+			shuffle=not self.stateful,
+			verbose=verbose
+		)
+
+		if self.stateful:
+			self.model.reset_states()
+
+	def plot_loss( self):
+		if( self.history is not None):
+			plt.figure()
+			plt.plot( self.history.history[ "loss"])
+			plt.xlabel( "Epochs")
+			plt.title( "Training Loss")
+			plt.show()
+		else:
+			print( "You Need to Call fit method first")
+
+	def predict( self, input, batch_size=1, verbose=0):
+		rnn = RNN(
+			neurons=self.neurons,
+			batch_size=batch_size,
+			window_size=self.window_size,
+			input_dim=self.input_dim,
+			stateful=self.stateful,
+			unroll=self.unroll
+		)
+
+		rnn.model.set_weights( self.model.get_weights())
+
+		y_pred = rnn.model.predict(
+			input,
+			batch_size=batch_size,
+			verbose=verbose
+		)
+
+		return y_pred
--- models/__init__.py
+++ models/__init__.py
@@ -5,3 +5,4 @@ tf.compat.v1.logging.set_verbosity( tf.compat.v1.logging.ERROR)
 
 from .SVR import SupportVectorRegressor
 from .Perceptron import Perceptron
+from .RNN import RNN
